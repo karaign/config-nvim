08152f4a2abb7fc63ce466ac0dfe71446f820c6a
Kara I <kara.ign@icloud.com>
2023-09-19 12:10:35 -0400

    Added stuff

--- init.lua
+++ init.lua
@@ -1,42 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -126,14 +87,15 @@ require('lazy').setup({
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+        vim.keymap.set('n', '<leader>gh', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview [H]unk' })
       end,
     },
   },
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+   'navarasu/onedark.nvim',
+    -- 'RRethy/nvim-base16',
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'onedark'
@@ -210,11 +172,11 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
--- See `:help vim.o`
+-- See `:help vim.uo`
 -- NOTE: You can change these options as you wish!
 
 -- Set highlight on search
@@ -252,7 +214,9 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
+vim.o.shell = '/usr/local/bin/fish'
 vim.o.termguicolors = true
+vim.keymap.set('t', '<Esc><Esc>', [[<C-\><C-n>]])
 
 -- [[ Basic Keymaps ]]
 
@@ -302,7 +266,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' } )
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
@@ -310,7 +274,7 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
+-- See `:help nvim-treesitter` 
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,68 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+local nvim_logo = {
+	[[                                   __                ]],
+	[[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	[[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	[[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	[[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	[[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+}
+
+local kitty = {
+	"          |\\      _,,,---,,_            ",
+	"    ZZZzz /,`.-'`'    -.  ;-;;,_        ",
+	"         |,4-  ) )-,_. ,\\ (  `'-'       ",
+	"        '---''(_/--'  `-'\\_)  Felix Lee "
+}
+
+local SESSION_DIR = "~/.config/nvim/sessions"
+
+return {
+  {
+    "startup-nvim/startup.nvim",
+    dependencies = {"nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim"},
+    config = function ()
+    	require("startup").setup()
+    end
+  },
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end
+  },
+
+  {
+    "rmagatti/auto-session",
+    config = function ()
+    	require("auto-session").setup {
+	log_level = "error",
+	auto_session_suppress_dirs = { "~/", "~/Downloads", "~/Developer", "/"}
+      }
+    end
+  },
+
+  {
+    "rmagatti/session-lens",
+    dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+    config = function ()
+    	require("telescope").load_extension("session-lens")
+	vim.keymap.set('n', '<leader>ss', require('session-lens').search_session, { desc = '[S]earch [S]essions' })
+    end
+  },
+
+  {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function ()
+      require("neoclip").setup()
+      require("telescope").load_extension("neoclip")
+      vim.keymap.set('n', '<leader>p', require("telescope").extensions.neoclip.neoclip, { desc = "[P]aste from history "})
+    end
+  }
+}
