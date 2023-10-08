9b0cee72db43129871904b2024c95fea0985c54f
Kara I <kara.ign@icloud.com>
2023-10-08 13:38:41 -0400

    Streamlined file organization

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-MY_COLORSCHEME = "kanagawa"
+MY_COLORSCHEME = "cattpuccin-macchiato"
 -- Neovide specific
 if vim.g.neovide then
-  require("init-neovide")
+  require("init.neovide")
 end
 
 -- make nvim shut up about the mouse
@@ -122,7 +122,7 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     dependencies = { 'ThemerCorp/themer.lua' },
-    config = require('init-lualine')
+    config = require('init.lualine')
     -- See `:help lualine.txt`
   },
 
@@ -183,20 +183,8 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  { import = 'custom.plugins' },
+    -- For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
+  { import = 'plugins' },
 }, {})
 
 -- [[ Setting options ]]
